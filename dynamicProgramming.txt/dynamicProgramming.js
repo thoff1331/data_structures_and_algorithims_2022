b68daad8943848b3b24398918dd5bbeca374c1ce
Trevor Hoffman <trevorrhoffman@gmail.com>
2022-10-08 22:13:58 +0000

    add dynamic programming, finished course

---
+++ dynamicProgramming.txt/dynamicProgramming.js
@@ -0,0 +1,27 @@
+function fib(n) {
+  if(n  <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+  
+}
+
+
+fib(5)
+
+
+function fib(n, memo=[]){
+  if(memo[n] !== undefined) return memo[n]
+  if(n <= 2) return 1;
+  var res = fib(n-1, memo) + fib(n-2, memo);
+  memo[n] = res;
+  return res;
+}
+
+
+function fib(n){
+    if(n <= 2) return 1;
+    var fibNums = [0,1,1];
+    for(var i = 3; i <= n; i++){
+        fibNums[i] = fibNums[i-1] + fibNums[i-2];
+    }
+    return fibNums[n];
+}
